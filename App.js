fd7cccbee83ab7a020b6e3fb7a1e546216bc2540
mishroom <mishall.afzal@gmail.com>
2018-06-03 17:13:10 -0700

    basic styling complete

--- App.js
+++ App.js
@@ -1,23 +1,97 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+} from 'react-native'
+
+let DeviceWidth = Dimensions.get('window').width
 
 export default class App extends React.Component {
+  renderBoard() {
+    let arr = new Array(9)
+    let DeviceWidth = Dimensions.get('window').width
+    return (
+      <View style={styles.grid}>
+        <View>
+          <GridItem position={1} value={'O'} style={{ marginBottom: 2 }} />
+          <GridItem position={2} value={'X'} style={{ marginBottom: 2 }} />
+          <GridItem position={3} value={null} style={styles.item} />
+        </View>
+        <View>
+          <GridItem
+            position={4}
+            value={null}
+            style={{ marginBottom: 2, marginLeft: 2 }}
+          />
+          <GridItem
+            position={5}
+            value={null}
+            style={{ marginBottom: 2, marginLeft: 2 }}
+          />
+          <GridItem position={6} value={null} style={{ marginLeft: 2 }} />
+        </View>
+        <View>
+          <GridItem
+            position={7}
+            value={null}
+            style={{ marginBottom: 2, marginLeft: 2 }}
+          />
+          <GridItem
+            position={8}
+            value={null}
+            style={{ marginBottom: 2, marginLeft: 2 }}
+          />
+          <GridItem position={9} value={null} style={{ marginLeft: 2 }} />
+        </View>
+      </View>
+    )
+  }
+  render() {
+    return <View style={styles.container}>{this.renderBoard()}</View>
+  }
+}
+
+class GridItem extends React.Component {
+  addMove() {
+    return null
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <TouchableOpacity
+        style={[styles.item, this.props.style]}
+        onPress={() => this.addMove.bind(this)}
+      >
+        <Text style={styles.text}>{this.props.value}</Text>
+      </TouchableOpacity>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  grid: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  item: {
+    width: DeviceWidth * 0.3,
+    height: DeviceWidth * 0.3,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 40,
+  },
+})
